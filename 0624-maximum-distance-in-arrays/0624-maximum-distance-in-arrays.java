08d9929ffb10dec5e9f1b0f99cf90a92e9e13a59
Nitesh J <jnitesh1463@gmail.com>
2024-08-16 10:12:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,16 +1,21 @@
 class Solution {
     public int maxDistance(List<List<Integer>> arr) {
         int mi = arr.get(0).get(0);
-        int ma = arr.get(0).get(arr.get(0).size() - 1);
-        int md = 0;
+        int ma = arr.get(0).get(arr.get(0).size()-1);
+        int res = 0;
+        int n1 = mi; int n2 = mi;
 
-        for (int i = 1; i < arr.size(); i++) {
-            md = Math.max(md, Math.abs(arr.get(i).get(arr.get(i).size() - 1) - mi));
-            md = Math.max(md, Math.abs(ma - arr.get(i).get(0)));
-            mi = Math.min(mi, arr.get(i).get(0));
-            ma = Math.max(ma, arr.get(i).get(arr.get(i).size() - 1));
+        for(int i = 1 ; i < arr.size(); i++) {
+            List<Integer> arr = arr.get(i);
+            int length = arr.size();
+            n1 = arr.get(0);
+            n2 = arr.get(length-1);
+            if (n2 - mi > res) res = n2 - mi;
+            if (ma - n1 > res) res = ma - n1;
+            if (n1 < mi) mi = n1;
+            if(n2 > ma) ma = n2;
         }
 
-        return md;  
+        return res;
     }
 }
