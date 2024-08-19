485feb534f7874783c6b9e67e77c8ef1fa8d5657
Nitesh J <jnitesh1463@gmail.com>
2024-08-19 23:06:47 +0530

    Time: 0 ms (100%), Space: 40.4 MB (65.41%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,15 +1,12 @@
 class Solution {
     public int minSteps(int n) {
-        int cur=1,res=0,cpy=0;
-        while(cur<n){
-            if(n%cur==0){
-                cpy=cur;
-                res++;
+        int res = 0;
+        for (int factor = 2; factor <= n; factor++) {
+            while (n % factor == 0) {
+                res += factor;
+                n /= factor;
             }
-            cur+=cpy;
-            res++;
         }
         return res;
-        
     }
 }
