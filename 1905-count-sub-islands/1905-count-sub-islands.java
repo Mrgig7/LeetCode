5744bc7468ba471235c38f57a0ad22a917aa2f55
Nitesh J <jnitesh1463@gmail.com>
2024-08-28 08:53:42 +0530

    Time: 19 ms (89.93%), Space: 83.8 MB (57.37%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,27 +1,27 @@
 class Solution {
-    int ans = 0;
-    public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int count = 0;
-        for(int i = 0 ; i < grid2.length ; i++){
-            for(int j = 0 ; j < grid2[0].length ; j++){
-			// applying dfs on grid2 and checking for corresponding cells in grid1 
-                if(grid2[i][j] == 1){
-                    ans = 1;
-                    dfs(grid1, grid2, i, j);
-                    count += ans;
+    int r = 0;
+    public int countSubIslands(int[][] g1, int[][] g2) {
+        int cnt = 0;
+        for(int i =0;i<g2.length;i++){
+            for(int j = 0;j<g2[0].length;j++){
+                if(g2[i][j]==1){
+                    r=1;
+                    helper(g1,g2,i,j);
+                    cnt+=r;
                 }
             }
         }
-        return count;
+        return cnt;
     }
-    public void dfs(int[][] grid1, int[][] grid2, int i, int j){
-        if(i < 0 || j < 0 || i >= grid1.length || j >= grid1[0].length || grid2[i][j] == 0) return ;
-		// if the corresponding cell in grid1 is water, then we just make ans = 0.
-        if(grid1[i][j] == 0) ans = 0;
-        grid2[i][j] = 0;
-        dfs(grid1, grid2, i-1, j);
-        dfs(grid1, grid2, i+1, j);
-        dfs(grid1, grid2, i, j+1);
-        dfs(grid1, grid2, i, j-1);
+
+    public void helper(int[][] g1, int[][] g2, int i, int j){
+        if(i<0||j<0||i>=g1.length||j>=g1[0].length||g2[i][j]==0) return;
+        if(g1[i][j]==0) r=0;
+        g2[i][j] = 0;
+        helper(g1,g2,i-1,j);
+        helper(g1,g2,i+1,j);
+        helper(g1,g2,i,j+1);
+        helper(g1,g2,i,j-1);
     }
+
 }
