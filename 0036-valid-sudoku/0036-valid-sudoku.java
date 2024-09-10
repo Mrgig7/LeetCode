62a9a07dbc796555d337ebaac1b7964dd749fc84
Nitesh J <jnitesh1463@gmail.com>
2024-09-11 01:30:05 +0530

    Time: 2 ms (79.15%), Space: 43.9 MB (96.1%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,16 +1,41 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        Set ch = new HashSet();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                if (board[i][j] != '.') {
-                    String el = "[" + board[i][j] + "]";
-                    if (!ch.add(el + i) || !ch.add(j + el) || !ch.add(i/3 + el + j/3))
-                        return false;
-                }
-
+    public boolean helper(char[][]board,int m,int n)
+    {
+        Set<Character> ch=new HashSet<>();
+        for(int i=m;i<m+3;i++){
+            for(int j=n;j<n+3;j++){
+                if(board[i][j]=='.') continue;
+                if(ch.contains(board[i][j])) return false;
+                ch.add(board[i][j]);
             }
         }
         return true;
+    }
+    
+    public boolean isValidSudoku(char[][] board) {
+        Set<Character> ch=new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(ch.contains(board[i][j])) return false;
+                ch.add(board[i][j]);
+            }
+            ch.clear();
+        }
+        for(int j=0;j<9;j++){
+            for(int i=0;i<9;i++){
+                if(board[i][j]=='.') continue;
+                if(ch.contains(board[i][j])) return false;
+                ch.add(board[i][j]);                
+            }
+            ch.clear();
+        }
+        for(int i=0;i<=6;i+=3){
+            for(int j=0;j<=6;j+=3){
+                if(!helper(board,i,j))return false;
+            }
+        }
+        return true;
+        
     }
 }
