a69915174e2952719e85f92774e629d39abe8fcb
Nitesh J <jnitesh1463@gmail.com>
2024-10-07 09:34:12 +0530

    Time: 1 ms (100%), Space: 42.8 MB (96.43%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -4,7 +4,7 @@ class Solution{
         int k = -1;
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(k == -1) arr[k++] = ch;
+            if(k == -1) arr[++k] = ch;
             else if(arr[k] == 'A' && ch == 'B') k--;
             else if(arr[k] == 'C' && ch == 'D') k--;
             else arr[++k] = ch;
