9100af670b06536a2bf2eb705e4c3d724193176b
Nitesh J <jnitesh1463@gmail.com>
2024-10-10 07:53:30 +0530

    Time: 3 ms (97.76%), Space: 54.9 MB (37.31%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.java
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -1,18 +1,19 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        int n = nums.length;
-        Integer[] idx = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            idx[i] = i;
+        int n = nums.length, res = 0;
+        int maidx[] = new int[n];
+        maidx[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--) {
+            maidx[i] = Math.max(maidx[i+1],nums[i]);
         }
-        Arrays.sort(idx, (a, b) -> Integer.compare(nums[a], nums[b]));
-        int ma = 0;
-        int midx = idx[0];
-        
-        for (int i = 1; i < n; i++) {
-            midx = Math.min(midx, idx[i]);
-            ma = Math.max(ma, idx[i] - midx);
+        int i =0, j = 1;
+        while(j<n){
+            if(nums[i]<=maidx[j]){
+                res = Math.max(res,j-i);
+                j++;
+            }
+            else i++;
         }
-        return ma;
+        return res;
     }
 }
