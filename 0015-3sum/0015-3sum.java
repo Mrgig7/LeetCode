97b6ae7256efab7b589a70b67821adeaf509419a
Nitesh J <jnitesh1463@gmail.com>
2024-10-14 23:56:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        //To prevent duplication
         Arrays.sort(nums);
-        Set<List<Integer>> list = new HashSet<>();
-        int s = 0;
-            for(int i = 0; i < nums.length-1; i++){
-                 int l = i + 1;
-                  int h = nums.length -1;
-            while(l < h){                
-                s = nums[i] + nums[l] + nums[h];
-                if(s == 0){
-                    list.add(Arrays.asList(nums[i], nums[l],nums[h]));
-                    l++;
-                    h--;
-                }
-                else if(s < 0){
-                    l++;
-                }
-                else{
-                    h--;
+
+        for(int i = 0; i<n-2; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1, k = n-1;
+            while(j<k){
+                int s = nums[i]+nums[j]+nums[k];
+                if(s==0){
+                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j] == nums[j+1]) j++;
+                    while(j<k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+                }else if(s>0){
+                    j++;
+                }else{
+                    k--;
                 }
             }
         }
-        return new ArrayList<>(list);
+
+        return res;
+
+
     }
 }
