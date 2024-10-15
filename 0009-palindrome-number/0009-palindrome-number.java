462b1e426d4410f20e182da0f17a66992c0878d3
Nitesh J <jnitesh1463@gmail.com>
2024-10-16 00:13:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-         int n=x;
-        int rev=0;
-        if(x<0)return false;
-        while(x>0){
-            rev=(rev*10)+(x%10);
-            x/=10;
+       //without converting into string
+        if(x<0 || x%10==0) return false;
+        if(x==0) return true;
+        if(x<10 ) return true;
+        int c = 0;
+        while(x>c){
+            c = c *10+ x%10;
+            x /= 10;
         }
-        if(rev==n)return true;
-        return false;
-        
+
+        return (x==c|| x == c/10);
+       
     }
 }
