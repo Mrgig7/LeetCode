d68a981d8e0f5ff82b5597eaeadaf0480a851566
Nitesh J <jnitesh1463@gmail.com>
2024-10-25 09:53:11 +0530

    Time: 42 ms (75.31%), Space: 55 MB (80.2%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> res = new ArrayList<>();
+        res.add(folder[0]);
+        for (int i = 1; i < folder.length; i++) {
+            String remf = res.get(res.size() - 1) + "/";
+            if (!folder[i].startsWith(remf)) res.add(folder[i]);
+        }
+        return res;
+    }
+}
