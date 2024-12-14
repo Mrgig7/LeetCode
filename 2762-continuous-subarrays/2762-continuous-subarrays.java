4b18c6aedcb9adffa0d6d8cd0661ada49ad8f827
Nitesh J <jnitesh1463@gmail.com>
2024-12-14 13:36:43 +0530

    Time: 22 ms (59.6%), Space: 56.5 MB (93.9%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -1,20 +1,37 @@
+import java.util.*;
+
 class Solution {
     public long continuousSubarrays(int[] nums) {
         int n = nums.length;
         long ans = 0;
-        for (int i = 0; i < n; i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for (int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                if (max - min <= 2) {
-                    ans++;
-                } else {
-                    break;
+        Deque<Integer> minDeque = new LinkedList<>();
+        Deque<Integer> maxDeque = new LinkedList<>();
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            while (!minDeque.isEmpty() && nums[minDeque.peekLast()] >= nums[right]) {
+                minDeque.pollLast();
+            }
+            minDeque.offerLast(right);
+
+            while (!maxDeque.isEmpty() && nums[maxDeque.peekLast()] <= nums[right]) {
+                maxDeque.pollLast();
+            }
+            maxDeque.offerLast(right);
+
+            while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > 2) {
+                left++;
+                if (minDeque.peekFirst() < left) {
+                    minDeque.pollFirst();
+                }
+                if (maxDeque.peekFirst() < left) {
+                    maxDeque.pollFirst();
                 }
             }
+
+            ans += right - left + 1;
         }
+
         return ans;
     }
 }
