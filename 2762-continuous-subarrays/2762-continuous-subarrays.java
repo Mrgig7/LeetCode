798a2052eebcb4ee60bd9413793231cbdc24b41c
Nitesh J <jnitesh1463@gmail.com>
2024-12-14 13:43:12 +0530

    Time: 25 ms (55.56%), Space: 56.4 MB (94.84%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -4,23 +4,31 @@ class Solution {
     public long continuousSubarrays(int[] nums) {
         int n = nums.length;
         long ans = 0;
+        
+        // Deques to store indices for the sliding window min and max
         Deque<Integer> minDeque = new LinkedList<>();
         Deque<Integer> maxDeque = new LinkedList<>();
+        
         int left = 0;
-
+        
         for (int right = 0; right < n; right++) {
+            // Maintain minDeque: the front of the deque is the minimum value index
             while (!minDeque.isEmpty() && nums[minDeque.peekLast()] >= nums[right]) {
                 minDeque.pollLast();
             }
             minDeque.offerLast(right);
-
+            
+            // Maintain maxDeque: the front of the deque is the maximum value index
             while (!maxDeque.isEmpty() && nums[maxDeque.peekLast()] <= nums[right]) {
                 maxDeque.pollLast();
             }
             maxDeque.offerLast(right);
-
+            
+            // Adjust left pointer when the condition max - min > 2
             while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > 2) {
                 left++;
+                
+                // Remove indices out of the window
                 if (minDeque.peekFirst() < left) {
                     minDeque.pollFirst();
                 }
@@ -28,10 +36,11 @@ class Solution {
                     maxDeque.pollFirst();
                 }
             }
-
+            
+            // Add the number of subarrays ending at 'right' to the answer
             ans += right - left + 1;
         }
-
+        
         return ans;
     }
 }
